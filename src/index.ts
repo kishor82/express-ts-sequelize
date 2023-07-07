4f4d64f36d4d5c2485a9f645288d956af1c10a94
kishor82 <kishorrathva8298@gmail.com>
2023-07-07 20:40:45 +0530

    feat: Added swagger support.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import http from "http";
-import { app } from "./app";
-import { config } from "./config";
+import http from 'http';
+import app from './app';
+import { config } from './config';
 
 const server = http.createServer(app);
 
